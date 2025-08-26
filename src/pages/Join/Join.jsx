49e5b0bc3b677d57443e45e7acb226a020542e77
AbdulkarimMziya <abdulmziya@gmail.com>
2025-08-26 15:38:52 -0700

    Fix: Add .jsx extension to RecruitForm import to resolve Netlify build error

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,4 +1,4 @@
-import RecruitForm from '../../Components/Forms/RecruitForm/RecruitForm';
+import RecruitForm from '../../Components/Forms/RecruitForm/RecruitForm.jsx';
 import styles from './Join.module.css';
 
 const Join = () => {
