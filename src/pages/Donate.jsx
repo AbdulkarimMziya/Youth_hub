aa7c4bbdbee1315a51fb77ecdbcc55fde5a36afc
AbdulkarimMziya <abdulmziya@gmail.com>
2025-09-12 10:33:28 -0700

    Add plain Donate page with basic styles

---
+++ src/pages/Donate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Donate.css';
+
+export default function Donate() {
+  return (
+    <div className="donate-container">
+      <h2>Donate</h2>
+      <p>Support our mission by making a donation. Thank you for your generosity!</p>
+    </div>
+  );
+}
