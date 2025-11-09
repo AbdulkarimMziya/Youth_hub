c9b534874d545b069877e653b734d95e6ff3e78c
AbdulkarimMziya <abdulmziya@gmail.com>
2025-11-08 18:02:33 -0800

    chore: add scroll-to-top in RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar.jsx';
 import Footer from '../Components/Footer';
 
 function RootLayout() {
+  const location = useLocation();
+
+  // Scroll to top (or to hash target) on route change
+  useEffect(() => {
+    // If there's a hash (e.g. #section), try to scroll to that element
+    if (location.hash) {
+      const id = location.hash.replace('#', '');
+      const el = document.getElementById(id) || document.querySelector(location.hash);
+      if (el && typeof el.scrollIntoView === 'function') {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        return;
+      }
+    }
+
+    // Default: scroll to top
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location.pathname, location.hash]);
+
   return (
     <>
       <Navbar />
